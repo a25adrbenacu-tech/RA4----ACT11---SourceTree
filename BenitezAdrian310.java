184e2199a0778a1760daf8fbc48d7f858f53db23
a25robhertam <a25robhertam@inspedralbes.cat>
2025-12-19 15:09:04 +0100

    Commit

--- BenitezAdrian310.java
+++ BenitezAdrian310.java
@@ -15,6 +15,19 @@ public class BenitezAdrian310 {
      */
     public static void main(String[] args) {
 
+
+
+
+
+        
+        for (int i = 0; i< 100; i++){
+            System.out.printf("por favor albert no toques main\n ");
+    
+    
+        }
+
+
+
         
         // TODO code application logic here
         Scanner input = new Scanner(System.in);
